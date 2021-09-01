eccf9a67042b8b4d0eab6c70c4253ec03b1f2ce4
yeyuqing <1542164273@qq.com>
2021-09-01 20:00:30 +0800

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container bg-white  dark:bg-gray-800">
       <section className="sidebar">侧边导航栏</section>
       <section className="main">
         <header className="header">
-          <span className="username">Hi, 安歌</span>
+          <span className="username text-blue-50">Hi, 安歌</span>
         </header>
-        <div className="wrapper">主体内容</div>
+        <div className="wrapper flex justify-center items-center">主体内容</div>
         <footer className="footer">
           <span className="copyright">Copyright@2019 安歌</span>
         </footer>
