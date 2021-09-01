9d364d3cbda646ad5d2ded0985f2a17d8c96ae08
yeyuqing <1542164273@qq.com>
2021-09-01 11:55:08 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="sidebar">侧边导航栏</section>
+      <section className="main">
+        <header className="header">
+          <span className="username">Hi, 安歌</span>
+        </header>
+        <div className="wrapper">主体内容</div>
+        <footer className="footer">
+          <span className="copyright">Copyright@2019 安歌</span>
+        </footer>
+      </section>
     </div>
   );
 }
